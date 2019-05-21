a55d877340ea86e9382123e6a7e29c12b60413d1
wanghongqiang <wanghongqiang@dpandora.cn>
2019-05-21 22:53:14 +0800

    test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,6 +5,7 @@ module.exports = {
     ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
   base: '/vuePressBlog/', // 这是部署到github相关的配置
+  dest: 'public',
   markdown: {
     lineNumbers: false // 代码块显示行号
   },
